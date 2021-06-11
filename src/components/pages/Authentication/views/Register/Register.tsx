36d35f15240e7427b0ceac74a75ed0cef607cbcc
Voragun Supawong <voragun2543@gmail.com>
2021-06-11 15:59:20 +0700

    [ADD] register form
    not complete register form

--- src/components/pages/Authentication/views/Register/Register.tsx
+++ src/components/pages/Authentication/views/Register/Register.tsx
@@ -4,6 +4,28 @@ function Register() {
     return (
       <div className="App">
         <h1>Register</h1>
+        <div>
+          <h1>สร้างบัญชี</h1>
+        
+        <form>
+          <div>
+            <input type="text" placeholder="ชื่อจริง"/>
+            <br></br>
+            <input type="text" placeholder="นามสกุล"/>
+            <br></br>
+            <input type="text" placeholder="ชื่อผู้ใช้" />
+            <br></br>
+            <input type="email" placeholder="อีเมล" />
+            <br></br>
+            <input type="password" placeholder="รหัสผ่าน" />
+            <br></br>
+            <input type="password" placeholder="ยืนยันรหัสผ่าน" />
+            <br></br>
+            <button>สร้างบัญชี</button>
+          </div>
+        </form>
+        
+        </div>
       </div>
     );
   }
