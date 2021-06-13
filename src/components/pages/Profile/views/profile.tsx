e044f05021ce823ccdb836b6fd97af7ccb3e3e74
veeraphatbew <82167572+veeraphatbew@users.noreply.github.com>
2021-06-13 18:57:51 +0700

    [ADD] Profile Form and user.json

--- src/components/pages/Profile/views/profile.tsx
+++ src/components/pages/Profile/views/profile.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import * as data from '../mocks/user.json';
 
 function Profile() {
     return (
       <div className="App">
-        <h1>Profile page</h1>
+        <h1>ข้อมูลส่วนตัว</h1>
+        <img></img>
+        <h2>ชื่อ-นามสกุล : {data['name-surname']}</h2>
+        <h2>อีเมล : {data.email}</h2>
+        <h2>ผลลัพธ์จากแบบทดสอบ : {data.result}</h2>
+
       </div>
     );
   }
