05371a34f85a760e952310d2eb5d3024b321865f
veeraphatbew <82167572+veeraphatbew@users.noreply.github.com>
2021-06-14 16:41:17 +0700

    [EDIT] Profile

--- src/components/pages/Profile/views/profile.tsx
+++ src/components/pages/Profile/views/profile.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
-//import { API_Profile_Data } from '../apis/profile.api'
-import * as data from '../mocks/user.json';
+//import * as data from '../mocks/user.json';
+//import axios from 'axios'
+import useSWR from 'SWR'
+import axios from 'axios';
 
+
+  
 function Profile() {
+    const fetcher = async (url: string) => {
+      const { data } = await axios.get(url);
+      if (data) {
+        return data;
+      }
+    };
+    const { data } = useSWR(
+      `http://api.randomuser.me/?inc=name,email`,
+      { fetcher: (url) => fetcher(url) },
+    );
+
     return (
       <div className="App">
         <h1>ข้อมูลส่วนตัว</h1>
-        <img></img>
-        <h2>ชื่อ-นามสกุล : {data['name-surname']}</h2>
+        <h2>ชื่อ-นามสกุล : {data.name}</h2>
         <h2>อีเมล : {data.email}</h2>
         <Link to="/editProfile"><button>แก้ไขข้อมูลส่วนตัว</button></Link>
-        <h2>ผลลัพธ์จากแบบทดสอบ : {data.result}</h2>
+        <h2>ผลลัพธ์จากแบบทดสอบ : </h2>
       </div>
     );
   }
 
 export default Profile;
-  
