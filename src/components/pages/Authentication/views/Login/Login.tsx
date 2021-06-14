037e3b53192efc841593c97240c73906ac2d748c
Voragun Supawong <voragun2543@gmail.com>
2021-06-14 16:44:13 +0700

    [UPDATE] layout
    not complete

--- src/components/pages/Authentication/views/Login/Login.tsx
+++ src/components/pages/Authentication/views/Login/Login.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { API_Login_Data } from '../../apis/login.api'
 import { Form, Input, Button, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Layout, Row, Col, Card } from 'antd';
 
 import 'antd/dist/antd.css';
 
@@ -22,6 +23,8 @@ const onFinishFailed = (errorInfo: any) => {
   console.log('Failed:', errorInfo);
 };
 
+const { Header, Footer, Content } = Layout;
+
 function Login() {
   async function getStatictisData() {
     const response = API_Login_Data()
@@ -36,60 +39,78 @@ function Login() {
   }, [])
   return (
 
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your Username!',
-            },
-          ]}
-        >
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="อีเมล" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your Password!',
-            },
-          ]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="รหัสผ่าน"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
+    <Form
+    name="normal_login"
+    className="login-form"
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    >
+
+      <Layout>
+        <Header style={{ textAlign: 'center' }}>
+          <h1 style={{ color: '#ffffff' }}>
+            เข้าสู่ระบบ
+          </h1>
+        </Header>
+      </Layout>
+
+      <Layout>
+        <Content>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="อีเมล"
+              style={{ width: 300 }}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="รหัสผ่าน"
+              style={{ width: 300 }}
+            />
           </Form.Item>
 
-        </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              เข้าสู่ระบบ
+            </Button>
+          </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            เข้าสู่ระบบ
-          </Button>
-          <br />
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Contnue as Guest
-          </Button>
-          <br />
-          ยังไม่มีบัญชีใช่ไหม? <a href="Register">สร้างบัญชีกันเถอะ!</a>
-        </Form.Item>
-      </Form>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Contnue as Guest
+            </Button>
+          </Form.Item>
+        </Content>
+      </Layout>
 
+      <Layout>
+        <Footer>
+          <Form.Item>
+            ยังไม่มีบัญชีใช่ไหม? <a href="Register">สร้างบัญชีกันเถอะ!</a>
+          </Form.Item>
+        </Footer>
+      </Layout>
+    </Form>
   );
 }
 
