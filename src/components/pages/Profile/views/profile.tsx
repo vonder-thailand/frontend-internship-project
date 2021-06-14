2cd82eb2d3d0f1fa4092d2eef9aa0cf3407b881f
veeraphatbew <82167572+veeraphatbew@users.noreply.github.com>
2021-06-14 18:29:25 +0700

    [ADD] swr fetcher

--- src/components/pages/Profile/views/profile.tsx
+++ src/components/pages/Profile/views/profile.tsx
@@ -1,30 +1,35 @@
 import { Link } from 'react-router-dom';
 //import * as data from '../mocks/user.json';
-//import axios from 'axios'
-import useSWR from 'SWR'
+import useSWR from 'swr'
 import axios from 'axios';
 
 
   
 function Profile() {
+    //const fetcher = (url: string) => axios.get(url).then(res => res.data);
     const fetcher = async (url: string) => {
       const { data } = await axios.get(url);
       if (data) {
         return data;
       }
     };
-    const { data } = useSWR(
-      `http://api.randomuser.me/?inc=name,email`,
-      { fetcher: (url) => fetcher(url) },
-    );
+    const { data , error} = useSWR(`https://jsonplaceholder.typicode.com/todos/1`,fetcher);
+    console.log(data , error);
+
+    if(error){
+      return <div>Error....</div>
+    }
+    if(!data){
+      return <div>Loading...</div>
+    }
 
     return (
       <div className="App">
         <h1>ข้อมูลส่วนตัว</h1>
-        <h2>ชื่อ-นามสกุล : {data.name}</h2>
-        <h2>อีเมล : {data.email}</h2>
+        <h2>ชื่อ-นามสกุล : {data.title}</h2>
+        <h2>อีเมล : {data.userId}</h2>
         <Link to="/editProfile"><button>แก้ไขข้อมูลส่วนตัว</button></Link>
-        <h2>ผลลัพธ์จากแบบทดสอบ : </h2>
+        <h2>ผลลัพธ์จากแบบทดสอบ : {data.id}</h2>
       </div>
     );
   }
