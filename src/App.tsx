f3c58a661e2b2c9c32ea428a3a907816fa891a8e
Nichkamon Promputta <fainichkamon@gmail.com>
2021-06-15 14:52:07 +0700

    [Test] test props style in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Routing from './routes/index';
 import styled from 'styled-components';
 import Burger from 'components/pages/Burger/Burger';
+import Flexbox from 'shared/style/theme/Flexbox';
+import Typography from 'shared/style/theme/Typograhy';
 
 const Container = styled.div`
     max-width: 600px;
@@ -12,10 +14,15 @@ const Container = styled.div`
 
 const App = () => {
     return (
+        <>
+        <Flexbox.Center>
+            <Typography.Body>5555555</Typography.Body>
+        </Flexbox.Center>
         <Container>
             <Burger />
             <Routing />
         </Container>
+        </>
     );
 };
 
